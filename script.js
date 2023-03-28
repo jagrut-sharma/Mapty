90a2fa7fe21af0cc2d78cc8478fe2b3db6cc4a53
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-03-28 14:29:08 +0530

    added marker functionality

--- script.js
+++ script.js
@@ -10,3 +10,51 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation?.getCurrentPosition(
+  function (pos) {
+    // console.log(pos);
+    const { latitude } = pos.coords;
+    const { longitude } = pos.coords;
+
+    // Creating the array of co-ordinates:
+    const coords = [latitude, longitude];
+
+    // Rendering map from leaflet:
+    const map = L.map('map').setView(coords, 13);
+
+    // An object of type e is stored in map
+    console.log(map); // e {options: {…}, _handlers: Array(7), _layers: {…}, _zoomBoundLayers: {…}, _sizeChanged: false, …}
+    // `https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png`
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Adding pointer where clicked functionality:
+    map.on('click', function (mapEvent) {
+      //   console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+      //   console.log(lat, lng);
+
+      // Adding a marker:
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 300,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    });
+  },
+  function () {
+    alert(`Could not find the position.`);
+  }
+);
